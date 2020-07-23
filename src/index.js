cb65bd5abd5c4f9880f7f591ebe3aa0a0b8fdfaf
Marvin_X <contact@marvinx.com>
2020-07-23 20:25:22 +0200

    animation greesock button nav

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
 import './assets/styles/styles.scss';
 
-const buttonNav = document.getElementById('button-nav');
-const nav = document.querySelector( 'nav[role=navigation]' );
+import {
+	gsap,
+	Core,
+	timelineLite,
+	timelineMax,
+	TimelineLite,
+	TimelineMax,
+	Plugins,
+	CSSPlugin,
+	AttrPlugin,
+	BezierPlugin,
+	DirectionalRotationPlugin,
+	RoundPropsPlugin,
+	Power0,
+	Power1,
+	Power2,
+	Power3,
+	Power4,
+	Back,
+	Bounce,
+	Circ,
+	Cubic,
+	Elastic,
+	Expo,
+	Linear,
+	Quad,
+	Quart,
+	Quint,
+	RoughEase,
+	Sine,
+	Strong,
+	SlowMo,
+	SteppedEase
+} from 'gsap/all';
 
-buttonNav.addEventListener( 'click', () => {
-  nav.classList.toggle('open');
-})
+const buttonNav = document.querySelectorAll('.button-nav');
+const nav = document.querySelector('nav[role="navigation"]');
 
+buttonNav.forEach( ( el, i ) => {
+  const iconRects = el.querySelectorAll( 'rect' );
+  const duration = parseFloat( window.getComputedStyle(nav, null ).getPropertyValue( "transition-duration" ) ) / 2;
+  const heightGroup = iconRects[i].parentElement.getBBox().height;
+  const heightLine = parseInt(iconRects[i].attributes.height.nodeValue);
+  const spaceLines = parseInt(heightLine + (heightGroup - heightLine*3) / 2);
 
+  const timeline = new TimelineMax( {
+    paused: true,
+    reversed: true,
+    ease : Quad.easeOut
+  } )
+    .to( iconRects[0], duration, { y: spaceLines }, 'position' )
+    .to( iconRects[2], duration, { y: -spaceLines }, 'position' )
+    .to( iconRects[0], duration, { rotation: 135, transformOrigin: '50% 50%', }, 'rotate' )
+    .to( iconRects[2], duration, { rotation: 225, transformOrigin: '50% 50%', }, 'rotate' )
+    .to( iconRects[1], duration, { scaleX: 0, transformOrigin: '50% 50%', }, 'rotate' );
+
+  el.addEventListener( 'click', ( e ) => {
+    timeline.reversed() ? timeline.play() : timeline.reverse();
+    nav.classList.toggle( 'open' );
+  });
+});
