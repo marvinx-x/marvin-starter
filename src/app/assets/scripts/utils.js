201f42a6e7082acff73dc1b62609a34c88a35c3e
Marvin_X <contact@marvinx.com>
2020-10-06 00:27:38 +0200

    begin forms

--- src/app/assets/scripts/utils.js
+++ src/app/assets/scripts/utils.js
@@ -19,9 +19,39 @@ function requireAll(r) {
   r.keys().forEach(r);
 }
 
+
+function inputFile() {
+  var inputs = document.querySelectorAll( '.input-file' );
+	Array.prototype.forEach.call( inputs, function( input )
+	{
+		var label	 = input.nextElementSibling,
+			labelVal = label.innerHTML;
+
+		input.addEventListener( 'change', function( e )
+		{
+			var fileName = '';
+			if( this.files && this.files.length > 1 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName )
+				label.querySelector( 'span' ).innerHTML = fileName;
+			else
+				label.innerHTML = labelVal;
+		});
+
+		// Firefox bug fix
+		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+	});
+}
+
+
 export function utils() {
   resize();
-  requireAll(require.context('../icons/', true, /\.svg$/));
+  requireAll( require.context( '../icons/', true, /\.svg$/ ) );
+  inputFile();
 }
 
 
